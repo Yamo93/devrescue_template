9a6e29b8b8733acd3dc86ad936ee9fb693b38da6
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2018-12-19 10:08:22 +0300

    added simple js functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', loadWeather);
+document.querySelector('.articles').addEventListener('click', foldArticle);
 
 function loadWeather() {
     let xhttp = new XMLHttpRequest();
@@ -16,4 +17,24 @@ function loadWeather() {
     }
     xhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=Malmoe&units=metric&APPID=cb95478d73e0c30d7837a71c615b03b4', true);
     xhttp.send();
+}
+
+let toggle = false;
+
+function foldArticle(e) {
+    if (e.target.classList.contains('read')) {
+        e.preventDefault();
+        
+        if (!toggle) {
+            e.target.previousElementSibling.style.height = '100%';
+            e.target.previousElementSibling.style.opacity = '1';
+            e.target.innerHTML = 'Läs mindre';
+            toggle = true;
+        } else {
+            e.target.previousElementSibling.style.height = '85vh';
+            e.target.previousElementSibling.style.opacity = '.7';
+            e.target.innerHTML = 'Läs mer';
+            toggle = false;
+        }
+    }
 }
